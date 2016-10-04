309b874df124d1cba27afbb9ef5a6e7a9da5852c
maxmaynard <maxwellvmaynard@gmail.com>
2016-10-03 18:50:16 -0700

    Adding assets necessary for ghostie units.

---
+++ Unity/CTIN485_AGD/Assets/mine/scenes/PlayerNavigation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNavigation : MonoBehaviour {
+
+    NavMeshAgent navigationAgent;
+
+	// Use this for initialization
+	void Start () {
+        navigationAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ClickToMove();
+	}
+
+    //Pretty self explanatory 
+    void ClickToMove()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit mouseClick;
+
+            if (Physics.Raycast(mouseRay, out mouseClick, 100))
+            {
+                navigationAgent.SetDestination(mouseClick.point);
+            }
+        }
+    }
+}  
